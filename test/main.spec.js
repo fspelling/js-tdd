cbe8ee226df0b78efd1b76ff2e8384becf04c20d
FERNANDO LENTISCO SPELLING <fernandpel@cptm.info>
2018-12-04 15:29:43 -0200

    start branch

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,64 +1,8 @@
-import { expect } from 'chai';
-import { sum, sub, mult, div } from '../src/main';
-
-describe('Calc', () => {
-    describe('smoke tests', () => {
-        it('Deveria existir a funcao sum', () => {
-            expect(sum).to.exist;
-            expect(sum).to.be.a('function');
-        });
-
-        it('Deveria existir a funcao sub', () => {
-            expect(sub).to.exist;
-            expect(sub).to.be.a('function');
-        });
-
-        it('Deveria existir a funcao mult', () => {
-            expect(mult).to.exist;
-            expect(mult).to.be.a('function');
-        });
-
-        it('Deveria existir a funcao div', () => {
-            expect(div).to.exist;
-            expect(div).to.be.a('function');
-        });
-    });
-
-    describe('Sum', () => {
-        it('Devera retornar 4 apos somar 2 com 2', () => {
-            let valor = sum(2, 2);
-            expect(valor).to.equals(4);
-        });
-    });
-
-    describe('Sub', () => {
-        it('Devera retornar 2 apos subtrair 1 com 3', () => {
-            let valor = sub(3, 1);
-            expect(valor).to.equals(2);
-        });
-
-        it('Devera retornar -4 apos subtrair 6 com 10', () => {
-            let valor = sub(6, 10);
-            expect(valor).to.equals(-4);
-        });
-    });
-
-    describe('Mult', () => {
-        it('Devera retornar 4 apos multiplicar 2 com 2', () => {
-            let valor = mult(2, 2);
-            expect(valor).to.equals(4);
-        });
-    });
-
-    describe('Div', () => {
-        it('Devera retornar 1 apos dividir 2 com 2', () => {
-            let valor = div(2, 2);
-            expect(valor).to.equals(1);
-        });
-
-        it('Devera retornar a mensagem "nao é possivel divisao com 0" apos dividir qualquer numero com 0', () => {
-            let valor = div(2, 0);
-            expect(valor).to.equals('nao é possivel divisao com 0');
-        });
-    });
-});
+/*
+ Desafio FizzBuzz
+ Escreva uma lib que receba um número e:
+ Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
+ Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
+ Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
+ Se não for múltiplo de nada, retorna o número
+*/
